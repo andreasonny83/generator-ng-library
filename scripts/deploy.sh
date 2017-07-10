91a9441e8e3d0b8e890e1e4f9855d3f7cd4828d3
Andrea Sonny <andreasonny83@gmail.com>
2017-07-10 21:24:23 +0100

    fix(postdeploy.sh): fixing post deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,36 +11,6 @@ set -e
 # Go to the project root directory
 cd $(dirname ${0})/..
 
-# VERSION_TYPE="${1:-patch}"
-
-# Commit a new tag
-# npm version ${VERSION_TYPE}
-# git push origin master --tags
-
-# Command line arguments.
-# COMMAND_ARGS=${*}
-
-PACKAGE_VERSION=$(cat ./package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
-# Write npmrc file for NPM publishing
-# rm -Rf ./dist/.npmrc
-
-# cat >> ./dist/.npmrc << EOF
-# always-auth=true
-# email=${NPM_EMAIL}
-# _auth=${NPM_TOKEN}
-# EOF
-
-echo "Deploying version: $PACKAGE_VERSION"
-
-# Update the package.json version
-sed -i "s/0.0.0-PLACEHOLDER/${PACKAGE_VERSION}/g" ./dist/package.json
-
 # Publish to NPM
 cd ./dist
 npm install
